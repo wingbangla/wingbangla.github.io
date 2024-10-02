d6532387b5888b95dd76ede6fccba2f7fab361c9
arifcseru <arifbd13@gmail.com>
2024-10-03 02:16:17 +0600

    ## adminpanel with features, ecom updated with cart and place order, website cms

--- adminpanel/service-worker.js
+++ adminpanel/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/adminpanel/precache-manifest.7706c23ee552744bb4938a41902a102b.js"
+  "/adminpanel/precache-manifest.0c30508985797b719ce80df90a689985.js"
 );
 
 self.addEventListener('message', (event) => {
