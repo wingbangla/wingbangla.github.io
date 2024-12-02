383c14f1cf56b8c1c7c4286e47046ccea38abef0
arifcseru <arifbd13@gmail.com>
2024-12-03 01:27:07 +0600

    ## release 1 upgraded

---
+++ adminpanel/precache-manifest.19074eb1d437d256e2b9d2c58356a64b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d8b78e2fdf501c683e36385d24fec2",
+    "url": "/adminpanel/index.html"
+  },
+  {
+    "revision": "95c0a739effb20ee0ac7",
+    "url": "/adminpanel/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "df9f8d5c2b9ddc844367",
+    "url": "/adminpanel/static/css/main.16060800.chunk.css"
+  },
+  {
+    "revision": "95c0a739effb20ee0ac7",
+    "url": "/adminpanel/static/js/2.4df74e13.chunk.js"
+  },
+  {
+    "revision": "8767853fd8673a3f0568f017b95000ef",
+    "url": "/adminpanel/static/js/2.4df74e13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9f8d5c2b9ddc844367",
+    "url": "/adminpanel/static/js/main.a97ad9c0.chunk.js"
+  },
+  {
+    "revision": "b1dfcc6a5f3f38059494",
+    "url": "/adminpanel/static/js/runtime-main.db1be315.js"
+  },
+  {
+    "revision": "3f4786c4d32598367b9174a672cad540",
+    "url": "/adminpanel/static/media/alfont_com_AlFont_com_DiodrumArabic-Bold-1.3f4786c4.ttf"
+  },
+  {
+    "revision": "78129f7e12d9169f857707e8125c721f",
+    "url": "/adminpanel/static/media/alfont_com_AlFont_com_DiodrumArabic-Semibold-1.78129f7e.ttf"
+  },
+  {
+    "revision": "02b19d2bf6b9467fa3735cd04c0edea9",
+    "url": "/adminpanel/static/media/companyLogo.02b19d2b.png"
+  },
+  {
+    "revision": "91a8ed67058d7bd7cc32976f46b92410",
+    "url": "/adminpanel/static/media/vendorLogo.91a8ed67.png"
+  }
+]);
--- adminpanel/service-worker.js
+++ adminpanel/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/adminpanel/precache-manifest.bf7a2d704bf02e20f16e4e7a38704a3b.js"
+  "/adminpanel/precache-manifest.19074eb1d437d256e2b9d2c58356a64b.js"
 );
 
 self.addEventListener('message', (event) => {
